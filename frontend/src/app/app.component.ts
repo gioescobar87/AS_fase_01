495e3855d88b5007d31e63cdc2c8f749ba82651f
gioescobar87 <descobarl4@miumg.edu.gt>
2024-07-30 22:48:34 -0600

    Modificación a componente principal

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderModule } from './header/header.module';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MenuComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend';
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'option1', component: ContentComponent, data: { content: 'Content for Option 1' }},
+  { path: 'option2', component: ContentComponent, data: { content: 'Content for Option 2' }},
+  { path: 'option3', component: ContentComponent, data: { content: 'Content for Option 3' }},
+  { path: '', redirectTo: '/option1', pathMatch: 'full' }
+];
