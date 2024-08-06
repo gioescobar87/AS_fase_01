0ea6128ae65ae33892decf0e03c8e91fb3ec6978
gioescobar87 <descobarl4@miumg.edu.gt>
2024-08-05 20:16:46 -0600

    Routing a componente gestion-admin

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { ContentComponent } from './content/content.component';
+import { GestionAdminComponent } from './gestion-admin/gestion-admin.component';
 
 export const routes: Routes = [
+  { path: 'gestion_admin', component: GestionAdminComponent },
   { path: 'option1', component: ContentComponent, data: { content: 'Content for Option 1' }},
-  { path: 'option2', component: ContentComponent, data: { content: 'Content for Option 2' }},
   { path: 'option3', component: ContentComponent, data: { content: 'Content for Option 3' }},
   { path: '', redirectTo: '/option1', pathMatch: 'full' }
 ];
