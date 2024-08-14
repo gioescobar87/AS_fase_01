9648797e4466c6edeb3b907ffb8e94872a021542
mario aquino <elchichoesungato@gmail.com>
2024-08-13 23:23:39 -0600

    agrega vista menusoportecomponente con su inport respectivo

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderModule } from './header/header.module';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
+import {MenuSoporteComponent} from './menu-soporte/menu-soporte.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MenuComponent],
+  imports: [RouterOutlet, HeaderComponent, MenuComponent,MenuSoporteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
