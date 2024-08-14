da2a509b0c085b51282b0d30a8a75d09e6408a42
mario aquino <elchichoesungato@gmail.com>
2024-08-13 23:22:22 -0600

    creacion de rutas para abrir subpaginas en soporte para dato_soporte y ticcket

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { ContentComponent } from './content/content.component';
 import { GestionAdminComponent } from './gestion-admin/gestion-admin.component';
-
+import {AdminTicketComponent} from './admin-ticket/admin-ticket.component';
+import { DatoSoporteComponent } from './dato-soporte/dato-soporte.component';
 export const routes: Routes = [
   { path: 'gestion_admin', component: GestionAdminComponent },
   { path: 'option1', component: ContentComponent, data: { content: 'Content for Option 1' }},
   { path: 'option3', component: ContentComponent, data: { content: 'Content for Option 3' }},
+  { path: 'option3', component: ContentComponent, data: { content: 'Content for Option 3' }},
+  { path: 'Tickets', component: AdminTicketComponent },
+  { path: 'sopor_dato', component: DatoSoporteComponent },
   { path: '', redirectTo: '/option1', pathMatch: 'full' }
 ];
