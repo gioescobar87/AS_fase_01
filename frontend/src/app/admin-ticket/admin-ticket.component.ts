533d313a5d97f1c7dee6e5c11da9b85f68660fd5
mario aquino <elchichoesungato@gmail.com>
2024-08-13 23:20:59 -0600

    creacion de componentes para tickets ninguna edicion con los demas

---
+++ frontend/src/app/admin-ticket/admin-ticket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-ticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-ticket.component.html',
+  styleUrl: './admin-ticket.component.css'
+})
+export class AdminTicketComponent {
+
+}
