6ce5cacbc145c4f9e67cc23feb1714fb88369a72
mario aquino <elchichoesungato@gmail.com>
2024-08-15 13:55:25 -0600

    creacion de nuevo componente ticekt de creacion

---
+++ frontend/src/app/crear-ticket/crear-ticket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-ticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './crear-ticket.component.html',
+  styleUrl: './crear-ticket.component.css'
+})
+export class CrearTicketComponent {
+
+}
